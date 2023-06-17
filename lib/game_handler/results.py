beb3c777b8fe5ffb6375e49fd855455a07c8ca61
d āvis <dlektauers@gmail.com>
2023-06-17 15:00:36 +0300

    Improve the results

--- lib/game_handler/results.py
+++ lib/game_handler/results.py
@@ -1,8 +1,28 @@
+import numpy as np
+
 import game_handler.data as data
 import algo_handler.hp
 
-def add(score) -> None:
-    data.results.append({
-        "score": score,
-        "hyperparameters": algo_handler.hp.get_named_values()
-    })
+def add(score: int) -> None:
+    hp_values = algo_handler.hp.get_named_values()
+
+    new_hyperparameters: bool = False
+    if len(data.results) == 0:
+        new_hyperparameters = True
+    else:
+        # Check if the current hyperparameters match the ones used in the last thing saved in results
+        # The code is super ugly and maybe can be improved but idc
+        for h in enumerate(list(data.results[-1]["hyperparameters"].values())):
+            if list(hp_values.values())[h[0]] != h[1]:
+                new_hyperparameters = True
+                break
+
+    if new_hyperparameters:
+        data.results.append({
+            "scores": [score],
+            "avg_score": score,
+            "hyperparameters": hp_values
+        })
+    else:
+        data.results[-1]["scores"].append(score)
+        data.results[-1]["avg_score"] = np.average(data.results[-1]["scores"])
